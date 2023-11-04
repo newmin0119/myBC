e3afff86aa6b1e3bbd3b0843386341fa13c4dc2c
newmin0119 <newmin0119@gmail.com>
2023-11-04 19:45:39 +0900

    [ADD] Class - Block, Block 생성, 머클트리 구성, 출력구문 작성

--- Blockchain/transaction.py
+++ Blockchain/transaction.py
@@ -2,6 +2,8 @@ import time
 from hashlib import sha256
 from random import random,randrange
 
+def HASH256(x):
+    return sha256(sha256(x.encode()).hexdigest().encode()).hexdigest()
 
 def make_transaction(input_pubkey,output_pubKey,Vid=None,modelName="",tradeCnt=1,price=0):
     a = {}
@@ -25,3 +27,50 @@ def make_transaction(input_pubkey,output_pubKey,Vid=None,modelName="",tradeCnt=1
 
 ### 예시 transaction
 print(make_transaction('seller pubkey','buyer pubkey',modelName='Genesis',price=30000000))
+
+class Block:
+    def __init__(self, args,transactions):
+        self.blockHeight=args['blockHeight']
+        self.prevHash   =args['prevHash']
+        self.nonce      =args['nonce']
+        self.Merkle_tree=self.__set_merkle(transactions)
+        self.Merkle_root=self.Merkle_tree[0]
+    
+    def __set_merkle(self,tx):
+        N = len(tx)
+        leaf_n = 1
+        while leaf_n<N: leaf_n<<=1
+        merkle_tree = ['0']*(leaf_n<<2)
+        for i in range(leaf_n):
+            if i<N:
+                merkle_tree[i+leaf_n]=HASH256(tx[i]['txid'])
+                merkle_tree[(i+leaf_n)<<1]=tx[i]
+            else:
+                merkle_tree[i+leaf_n]=HASH256(tx[N-1]['txid'])
+        while leaf_n>1:
+            leaf_n>>=1
+            for i in range(leaf_n):
+                merkle_tree[i+leaf_n]=HASH256(merkle_tree[(i+leaf_n)<<1]+merkle_tree[((i+leaf_n)<<1)+1])
+        return merkle_tree
+    
+    def __str__(self) -> str:
+        return str({
+            'Header':{
+                'blockHeight':self.blockHeight,
+                'prevHash':self.prevHash,
+                'nonce':self.nonce,
+                'Merkle-root':self.Merkle_root
+            },
+            'transactions':self.Merkle_tree
+        })
+
+### 예시 Block
+tx = []
+for i in range(5):
+    tx.append(make_transaction('seller pubkey','buyer pubkey',modelName='Genesis',price=i))
+block_arg = {
+    'blockHeight':0,
+    'prevHash':'asdfasdfadsfasdfasdfasdfasdf',
+    'nonce':'fdsafdsafdsafdsa',
+}
+print(Block(block_arg,transactions=tx))
