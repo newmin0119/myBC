e5d2c8e8ababe399bd9daa68878b370298cdc317
newmin0119 <newmin0119@gmail.com>
2023-11-07 14:35:16 +0900

    [FEAT] Crypto_tools.py 파일명 변경 및 서명함수 추가

--- Blockchain/Crpto_tools.py
+++ Blockchain/Crpto_tools.py
@@ -5,12 +5,17 @@ from ecdsa.util import sigencode_string,sigdecode_string
 def HASH256(x):
     return sha256(sha256(x.encode()).hexdigest().encode()).hexdigest()
 
-# 서명 인증 함수 -- 구현필요
-def verify_sig(sig, pk_str, message):
+# 서명 생성 함수
+def create_sig(sk,txid):
+    message = bytes(txid.encode()) # byte 형식 message 샘플
+    sig = sk.sign_deterministic(message,hashfunc=sha256,sigencode=sigencode_string)
+    return sig
+
+# 서명 인증 함수
+def verify_sig(sig, pk_str, message) -> bool:
     '''
-    ECDSA 기반 서명 검증 결과 -> message (str)
+    ECDSA 기반 서명 검증 결과 -> bool
     ECDSA 기반 sig, pk로 검증
     '''
     pk = VerifyingKey.from_string(pk_str) # str에서 pubkey 보구
     return pk.verify(sig,message,sha256,sigdecode=sigdecode_string)
-    # message 반환형태 가공
