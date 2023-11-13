04bb91f51aa1f9da604fd08da2827adf943dcd73
newmin0119 <newmin0119@gmail.com>
2023-11-13 15:37:50 +0900

    [FEAT] PRNG 이용한 P2P 구성

--- MasterProcess/master_process.py
+++ MasterProcess/master_process.py
@@ -9,32 +9,59 @@ from Blockchain.Blocks import Block
    
 
 Target_N = '0000080000000000000000000000000000000000000000000000000000000000'
+usernodes = []
+fullnodes = []
+Master_from_full = []
+def construct_P2P(N,M):
+    '''
+    가상 P2P Network를 구성하는 function
+    매개변수 N과 M은 각각 fullnode의 수와 usernode의 수로 대응
 
-def construct_P2P(N,M,fullnodes,usernodes,listen_Block):
-    link_User_to_Full = []
-    check = []
-    for u_node in range(M):
-        f_node = randint(0,N-1)
-        while f_node in check:
-            f_node = randint(0,N-1)
-        readP, writeP = Pipe()
-        link_User_to_Full.append((u_node,f_node,writeP,readP))
-        check.append(f_node)
-    
-    link_Master_toFull = []
-    
-    for _ in range(N):
-        read_M, write_F = Pipe()
-        link_Master_toFull.append(write_F)
-        listen_Block.append(read_M)
-    for u,f,w,r in link_User_to_Full:
-        usernodes[u] = UserNode_Process(write_pipe=w)
-        fullnodes[f] = FullNode_Process(genesis,Target_N,'192.0.0.'+str(f),8333,r,'여기에는 풀노드 임의 링크',link_Master_toFull[f])
-    
-    for f in range(N):
-        if f not in check:
-            fullnodes[f] = FullNode_Process(genesis=genesis,target=Target_N,read_pipe=None,write_pipe=link_Master_toFull[f])
+    PRNG를 이용하여
+     - 각 UserNode는 임의의 FullNode로         Pipe를 이용, 단방향 연결:    user_to_full, full_from_user
+     - 각 FullNode는 임의의 다른 FullNode로     Socket을 이용, 양방향 연결:  full_to_full   
+     - 그리고 모든 FullNode는 MasterProcess로   Pipe를 이용, 단방향 연결:   full_to_master, mastter_from_full
 
+    이후, 생성된 Pipe와 Socket을 각각 User와 Full Node class로 argument로 전달
+    이때, 임의의 ip와 port번호 ( 여기서는 Bitcoin의 port번호 8333을 이용 )를 같이 전달
+    '''
+    user_to_full = []
+    full_from_user = [list()]*N
+    full_to_full = [list()]*N
+    full_to_Master = []
+    
+    # User <-> Full 구성
+    for user_num in range(M):
+        full_num = randint(0,M-1)
+        read_pipe,write_pipe = Pipe()
+        user_to_full.append(write_pipe)
+        full_from_user[full_num].append(read_pipe)
+
+    # Full <-> Full 구성
+    # 이중 for문과 randint 0,1을 이용 50%의 확률로 두 노드 사이의 연결이 있는 지 없는 지 결정
+    for full_A in range(N-1):
+        for full_B in range(full_A+1,N):
+            if randint(0,1):
+                full_to_full[full_A].append(full_B)
+                full_to_full[full_B].append(full_A)
+    
+    # Full <-> Master 구성
+    for full_num in range(N):
+        read_pipe, write_pipe = Pipe()
+        full_to_Master.append(write_pipe)
+        Master_from_full.append(read_pipe)
+
+
+    # usernode Process 생성
+    for user_num in range(M):
+        usernodes[user_num] = UserNode_Process(write_pipe=user_to_full(user_num))
+
+    # fullnode Process 생성
+    for full_num in range(N): 
+        fullnodes[full_num] = FullNode_Process(genesis,Target_N,'192.0.0.'+str(f),8333,
+                                               full_from_user[full_num],
+                                               full_to_Master[full_num])
+    
 if __name__=='__main__':
     manager = Manager()
     
@@ -48,12 +75,9 @@ if __name__=='__main__':
 
     print('Genests: \n',genesis,end='\n\n')
     N,M = map(int,input().split())
-    fullnodes = [None]*N
-    usernodes = [None]*M
-    listen_Block = []
 
-    construct_P2P(N,M,fullnodes,usernodes,listen_Block)
-
+    construct_P2P(N,M)
+    
     while True:
         for node in fullnodes:
             node.start()
